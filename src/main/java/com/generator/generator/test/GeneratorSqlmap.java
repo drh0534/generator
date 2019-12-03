725cd0e0f12fce9271257068af21cf9caf99fa3d
wupeiwen <wuaiyishui521>
2019-12-03 15:20:22 +0800

    update

--- src/main/java/com/generator/generator/test/GeneratorSqlmap.java
+++ src/main/java/com/generator/generator/test/GeneratorSqlmap.java
@@ -6,6 +6,7 @@ import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,14 +26,39 @@ public class GeneratorSqlmap {
 		myBatisGenerator.generate(null);
 
 	}
-	public static void main(String[] args) throws Exception {
-		try {
-			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
-			generatorSqlmap.generator();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//	public static void main(String[] args) throws Exception {
+//		try {
+//			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+//			generatorSqlmap.generator();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
 
+	public static void main(String[] args) throws Exception{
+		GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+
+		generatorSqlmap.generator2();
+		System.out.println(System.getProperty("user.dir"));
+	}
+
+	public void generator2() throws Exception{
+
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("F:/lecar-work/generator/src/main/resources/generatorConfig.xml");
+		InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+//		Configuration config = cp.parseConfiguration(resourceAsStream);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+
+		for(String warning:warnings){
+			System.out.println(warning);
+		}
 	}
 
 
